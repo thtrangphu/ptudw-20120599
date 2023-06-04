63ca9d08b56c5d1a74e04a61a673dc279590e663
Phù Thị Kim Trang <76843467+thtrangphu@users.noreply.github.com>
2023-06-04 22:08:10 +0700

    update jwt and mailjet

---
+++ controllers/jwt.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "S3cret";
+
+function sign(email, expiresIn = "30m") {
+  return jwt.sign({ email }, process.env.JWT_SECRET || JWT_SECRET, {
+    expiresIn,
+  });
+}
+
+function verify(token) {
+  try {
+    jwt.verify(token, process.env.JWT_SECRET || JWT_SECRET);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+module.exports = { sign, verify };
